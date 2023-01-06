84b94529810b0deb96f5deb182580d1158872b22
VideoMem <sebastian.wilwerth@gmail.com>
2023-01-05 21:35:57 -0300

    adds modpoll notes

--- termohigrometer-modbus.ino
+++ termohigrometer-modbus.ino
@@ -60,6 +60,17 @@ uint8_t analog_pins[] = {A0, A1, A2, A3, A4, A5};        // Add the pins you wan
 // First 10 registers are for time on values for each drive
 // The same applies for the following 10 that are for the time off values.
 
+// # factoryDefaults setings using modpoll tool
+//
+// modpoll -b 9600 -m rtu -a 1 -t 4:int -r 101 -c 20  -p none -4 1  /dev/ttyUSB0 300000 300000 300000 300000 300000 300000 300000 300000 300000 500 300000 300000 300000 300000 300000 300000 300000 300000 300000 500
+//
+// # change led blink on time using modpoll tool (first value lsb word 16 bit, second value high msb word), the example is 200 ms
+//
+// modpoll -b 9600 -m rtu -a 1 -t 4:hex  -r 137 -c 2  -p none -4 1  /dev/ttyUSB0 200 0
+//
+
+
+
 // You shouldn't have to change anything below this to get this example to work
 
 uint8_t digital_pins_size = sizeof(digital_pins) / sizeof(digital_pins[0]); // Get the size of the digital_pins array
